92a1121ae0f55f40ab5bfa8ff30440e35b1dabf3
Ayush4NattyHatty <dhanrajaayush123@gmail.com>
2023-10-04 11:35:42 +0530

    Add topbar dropdown

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,17 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import getURIFromIcon from "../../utility/getURIFromIcon";
 import styles from "./styles.module.css";
 import {
   AiOutlineSearch,
   AiOutlineCalendar,
   AiOutlineDown,
+  AiOutlineMail,
 } from "react-icons/ai";
 
 export default function Topbar() {
   const interviewDateRef = useRef();
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
   const handleInterviewDateClick = () => {
     // @ts-ignore
     if (interviewDateRef.current) interviewDateRef.current.showPicker();
+  };
+
+  const handleOnClickAvatar = () => setShowDropdown(true);
+  const handleCloseDropdown = () => {
+    if (showDropdown) setShowDropdown(false);
   };
 
   return (
@@ -43,7 +51,22 @@ export default function Topbar() {
           <AiOutlineDown />
         </div>
         <div className={styles.divider}></div>
-        <div className={styles.avatar}></div>
+        <div className={styles.avatar} onClick={handleOnClickAvatar}>
+          {showDropdown && (
+            <div className={styles.dropdown} onMouseLeave={handleCloseDropdown}>
+              <div className={styles.dp}></div>
+              <div className={styles.details}>
+                <div>
+                  <p className={styles.text}>Name</p>
+                  <p className={styles.text}>Location</p>
+                </div>
+                <button className={styles.email}>
+                  <AiOutlineMail />
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
